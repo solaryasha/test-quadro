7cf3e1c09fdfca1f34867c84b843e012ea4dc37a
Oleksandr Ivanchenko <101979538+Oleksandr-Ivanchenko@users.noreply.github.com>
2022-09-23 09:50:04 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import "./styles.css";
+
+let detail = {
+  length: 350,
+  width: 250,
+  lt: { radius: 0 },
+  lb: { radius: 0 },
+  rt: { radius: 50 },
+  rb: { radius: 0 }
+};
+
+{
+  const app = document.getElementById("app");
+  const heightInput = document.getElementById("heightInput");
+  const widthInput = document.getElementById("widthInput");
+  const leftAngleInput = document.getElementById("leftAngleInput");
+  const rotateZ = document.getElementById("rotateZ");
+
+  //your code here....
+  
+}
+
